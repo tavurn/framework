633a813e3377a1e69f5003518014a185fac5dd02
David <75451291+dulkoss@users.noreply.github.com>
2023-09-17 15:36:44 +0200

    add ServiceProviders to Application

--- src/Application.php
+++ src/Application.php
@@ -3,22 +3,89 @@
 namespace Tavurn;
 
 use OpenSwoole\Core\Psr\Response;
+use OpenSwoole\Server;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Tavurn\Async\Coroutine;
+use Tavurn\Container\Container;
+use Tavurn\Support\ServiceProvider;
 
-class Application implements RequestHandlerInterface
+class Application extends Container implements RequestHandlerInterface
 {
     protected static Application $instance;
 
-    public function __construct()
+    protected Server $server;
+
+    /**
+     * @var array<int, ServiceProvider>
+     */
+    protected array $providers = [];
+
+    public function __construct(Server $server)
     {
         static::$instance = $this;
+
+        $this->server = $server;
+
+        $this->register(static::getCoreProviders());
+    }
+
+    public static function getCoreProviders(): array
+    {
+        return [
+            \Tavurn\Providers\EventServiceProvider::class,
+        ];
     }
 
     public static function instance(): self
     {
         return self::$instance;
+    }
+
+    /**
+     * @param array<int, string>|string $providers
+     * @return $this
+     */
+    public function register($providers): static
+    {
+        if (! is_array($providers) && $providers) {
+            $providers = [$providers];
+        }
+
+        $instanced = array_map(fn ($provider) => new $provider($this), $providers);
+
+        foreach ($instanced as $provider) {
+            $provider->register();
+        }
+
+        $this->providers = array_merge($this->providers, $instanced);
+
+        return $this;
+    }
+
+    public function getServer(): Server
+    {
+        return $this->server;
+    }
+
+    public function bootServiceProviders(array $providers): void
+    {
+        Coroutine::each(
+            $providers,
+            fn (ServiceProvider $provider) => $provider->booting(),
+        );
+    }
+
+    public function start(): never
+    {
+        $this->bootServiceProviders($this->providers);
+
+        $this->server->setHandler($this);
+
+        $this->server->start();
+
+        exit('The server has stopped'.PHP_EOL);
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
