f2e626b82f881b911286a2aaaed3df9d900a730f
David <75451291+dulkoss@users.noreply.github.com>
2023-09-17 15:07:27 +0200

    add Event Facade

---
+++ src/Facades/Event.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tavurn\Facades;
+
+use Tavurn\Contracts\Events\Dispatcher;
+use Tavurn\Support\Facade;
+
+/**
+ * @method static \object dispatch(\object $event)
+ */
+class Event extends Facade
+{
+    protected static function getContainerAccessor(): string
+    {
+        return Dispatcher::class;
+    }
+}
