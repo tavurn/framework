ab9f6814cd17343707f6f1c7c3b041a9e539b433
David <75451291+dulkoss@users.noreply.github.com>
2023-09-29 22:59:00 +0200

    update Facade to not statically store the same underlying object for all Facades

--- src/Support/Facade.php
+++ src/Support/Facade.php
@@ -4,16 +4,10 @@ namespace Tavurn\Support;
 
 abstract class Facade
 {
-    protected static mixed $instance;
-
     abstract protected static function getContainerAccessor(): string;
 
     public static function __callStatic(string $name, array $arguments): mixed
     {
-        if (! isset(static::$instance)) {
-            static::$instance = app(static::getContainerAccessor());
-        }
-
-        return static::$instance->{$name}(...$arguments);
+        return app(static::getContainerAccessor())->{$name}(...$arguments);
     }
 }
