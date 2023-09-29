024a38d1bc8c817486060a17607b41d05205ebdd
David <75451291+dulkoss@users.noreply.github.com>
2023-09-29 23:45:22 +0200

    add Exception handler facade

---
+++ src/Facades/Exception.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tavurn\Facades;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Tavurn\Support\Facade;
+
+/**
+ * @method static \void report(\Throwable $error)
+ * @method static \bool shouldReport(\Throwable $error)
+ * @method static ResponseInterface render(ServerRequestInterface $request, \Throwable $error)
+ */
+class Exception extends Facade
+{
+    protected static function getContainerAccessor(): string
+    {
+        return \Tavurn\Contracts\Exceptions\Handler::class;
+    }
+}
