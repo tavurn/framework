0b37e183f93c234156313b8b3a7930188d604c1a
David <75451291+dulkoss@users.noreply.github.com>
2023-11-01 20:26:41 +0100

    bootstrap console kernel and add automatic command loading (based on path)

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -2,10 +2,12 @@
 
 namespace Tavurn\Console;
 
+use Illuminate\Support\Str;
 use Symfony\Component\Console\Application as SymfonyApplication;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface as Input;
 use Symfony\Component\Console\Output\OutputInterface as Output;
+use Symfony\Component\Finder\Finder;
 use Tavurn\Contracts\Console\Kernel as KernelContract;
 use Tavurn\Contracts\Foundation\Application;
 
@@ -19,11 +21,6 @@ class Kernel implements KernelContract
      * @var array<int, class-string>
      */
     protected static array $bootstrappers = [];
-
-    /**
-     * @var array<int, Command>
-     */
-    protected array $commands = [];
 
     protected bool $commandsLoaded = false;
 
@@ -42,6 +39,8 @@ class Kernel implements KernelContract
             $this->commandsLoaded = true;
         }
 
+        $this->bootstrap();
+
         return $this->symfony->run($input, $output);
     }
 
@@ -50,12 +49,54 @@ class Kernel implements KernelContract
         //
     }
 
+    protected function load(string $path)
+    {
+        foreach ((new Finder)->files()->in($path) as $file) {
+            $name = $this->classNameFromFile($file);
+
+            if (
+                ! is_subclass_of($name, Command::class) &&
+                ! (new \ReflectionClass($name))->isAbstract()
+            ) {
+                continue;
+            }
+
+            $this->symfony->add(
+                $this->makeCommand($name),
+            );
+        }
+    }
+
+    protected function classNameFromFile(\SplFileInfo $file): string
+    {
+        $rel = Str::after(
+            $file->getRealPath(),
+            realpath(app()->basePath()) . DIRECTORY_SEPARATOR,
+        );
+
+        $name = str_replace(
+            ['/', '.' . $file->getExtension()],
+            ['\\', ''],
+            $rel,
+        );
+
+        return Str::studly($name);
+    }
+
+    /**
+     * @param class-string<Command> $class
+     */
+    protected function makeCommand(string $class): Command
+    {
+        return $this->app->make($class);
+    }
+
     protected function bootstrap(): void
     {
         if (! $this->app->hasBeenBootstrapped()) {
             $bootstrappers = array_merge(
                 static::$bootstrappers,
-                $this->app::$boostrappers ?? [],
+                $this->app::$bootstrappers ?? [],
             );
 
             $this->app->bootstrapWith($bootstrappers);
